14f298ffbf1994822b28f73fbaabbe76c0037750
raulsan <ufreeurself@gmail.com>
2010-11-11 13:30:43 -0600

    my app raulsan in 3.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Raulsan::Application.config.secret_token = '25f4a49d419dcc65610daf855b46e54a3f826151adc06e035bf9306fd740222a646be74f716aef18f47c35f0d112bb1555edb8654b08e853d82e58a88747aa55'
